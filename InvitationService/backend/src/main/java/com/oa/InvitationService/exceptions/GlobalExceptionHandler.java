815a6b333aa8a449d83bf7a4645b4ff027ae637d
OmarAyman1234 <omarayman9124@gmail.com>
2026-05-05 16:12:17 +0300

    Added extra exceptions to the handler

--- InvitationService/backend/src/main/java/com/oa/InvitationService/exceptions/GlobalExceptionHandler.java
+++ InvitationService/backend/src/main/java/com/oa/InvitationService/exceptions/GlobalExceptionHandler.java
@@ -3,10 +3,14 @@ package com.oa.InvitationService.exceptions;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -23,4 +27,20 @@ public class GlobalExceptionHandler {
         });
         return errorMap;
     }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND) // This turns the 500 into a 404
+    public Map<String, String> handleEntityNotFound(EntityNotFoundException e) {
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("error", e.getMessage());
+        return errorMap;
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST) // Use 400
+    public Map<String, String> handleIllegalState(IllegalStateException e) {
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("error", e.getMessage());
+        return errorMap;
+}
 }
