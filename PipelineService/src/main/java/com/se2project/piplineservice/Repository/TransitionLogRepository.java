f5422065714d6584418562b9ab284b7df608e369
sara400m <sarah_20230249@fci.helwan.edu.eg>
2026-05-07 16:57:17 +0300

    TransitionLogRepository.java

--- PipelineService/src/main/java/com/se2project/piplineservice/Repository/TransitionLogRepository.java
+++ PipelineService/src/main/java/com/se2project/piplineservice/Repository/TransitionLogRepository.java
@@ -1,9 +1,9 @@
 package com.se2project.piplineservice.Repository;
-package com.se2project.piplineservice.model.TransitionLog;
 
-import jakarta.persistence.*;
+import com.se2project.piplineservice.model.TransitionLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TransitionLogRepository extends JpaRepository<TransitionLog, Long> {
 }
-
