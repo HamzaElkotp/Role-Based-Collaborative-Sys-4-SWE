20146e468b25b6f319779f7814e296e9a3972717
sara400m <sarah_20230249@fci.helwan.edu.eg>
2026-05-07 16:58:18 +0300

    UpdateRepository.java

--- PipelineService/src/main/java/com/se2project/piplineservice/Repository/UpdateRepository.java
+++ PipelineService/src/main/java/com/se2project/piplineservice/Repository/UpdateRepository.java
@@ -1,7 +1,9 @@
 package com.se2project.piplineservice.Repository;
-package com.se2project.piplineservice.model.Update;
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.se2project.piplineservice.entity.Update;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface UpdateRepository extends JpaRepository<Update, Long> {
 }
