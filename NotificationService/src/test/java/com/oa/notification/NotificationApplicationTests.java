3420f7a16a0ecfa4b692496b38c9649a698a638f
OmarAyman1234 <omarayman9124@gmail.com>
2026-05-08 16:24:55 +0300

    Initializing NotificationService

---
+++ NotificationService/src/test/java/com/oa/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oa.NotificationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
